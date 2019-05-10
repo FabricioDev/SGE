fcaf736c5349eb72d1213b8ba5bc644d1ca3d101
Fabrício M. Damasceno <fabricio_wm@hotmail.com>
2019-05-09 23:50:18 -0300

    Adicionando usuários padrões na Seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,11 +12,25 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        //Usuários padrões do sistema
+        //Pode logar com o e-mail ou matricula
         factory(User::class)->create([
             'name' => 'Admin',
             'email' => 'admin@user.com',
             'password' => bcrypt('s3cr3t'),
             'enrolment' => 100000,
+        ]);
+        factory(User::class)->create([
+            'name' => 'Fabricio M. Damasceno',
+            'email' => 'fabricio.devwebrj@gmail.com',
+            'password' => bcrypt('s3cr3t'),
+            'enrolment' => 100001,
+        ]);
+        factory(User::class)->create([
+            'name' => 'Uatson',
+            'email' => 'jaguarinternet@gmail.com',
+            'password' => bcrypt('J@gu@r'),
+            'enrolment' => 100002,
         ]);
     }
 }
