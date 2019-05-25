db083f535e80db55e25a819c2e8492882e130785
Fabrício M. Damasceno <fabricio_wm@hotmail.com>
2019-05-25 01:58:35 -0300

    Criando Abas para navegação

--- resources/views/admin/users/profile/edit.blade.php
+++ resources/views/admin/users/profile/edit.blade.php
@@ -3,15 +3,18 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <h3>Editar Perfil</h3>
-            <?php $icon = Icon::create('pencil');?>
-            {!!
-                form($form->add('salve', 'submit', [
-                    'attr' => ['class' => 'btn btn-primary btn-block'],
-                    'label' => $icon
-                ]))
-            !!}
+            @component('admin.users.tabs-component',['user' => $form->getModel()])
+                <div class="col-md-12">
+                    <h3>Editar Perfil</h3>
+                    <?php $icon = Icon::create('pencil');?>
+                    {!!
+                        form($form->add('salve', 'submit', [
+                            'attr' => ['class' => 'btn btn-primary btn-block'],
+                            'label' => $icon
+                        ]))
+                    !!}
+                </div>
+            @endcomponent
         </div>
     </div>
-    <br>
 @endsection
